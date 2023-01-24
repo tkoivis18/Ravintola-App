7e84ae2500bcda9ff409186b6265a236ab07991e
tkoivis18 <tobias.koivisto@gmail.com>
2023-01-24 14:13:01 +0200

    infrastructure folder with themes

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -3,6 +3,15 @@ import styled from "styled-components/native";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
+const RestaurantCard = styled(Card)`
+  backgroundcolor: "white";
+`;
+
+const restaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  backgroundcolor: "white";
+`;
+
 const Title = styled.Text`
   padding: 16px;
   color: red;
@@ -22,14 +31,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <Card elevation={5}>
+      <Card.Cover key={name} source={{ uri: photos[0] }} />
       <Title>{name}</Title>
     </Card>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
