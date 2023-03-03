6f6920969dbc412e5b40dc4c587e4873fae41f18
tkoivis18 <tobias.koivisto@gmail.com>
2023-03-03 23:25:33 +0200

    created fake API request

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,5 +1,17 @@
 import { mocks } from "./mock";
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
-  console.log(mocks);
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("not found");
+    }
+    resolve(mock);
+  });
 };
-restaurantsRequest();
+restaurantsRequest()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
